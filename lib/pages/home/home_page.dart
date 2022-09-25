62eb5c7329226a5510069c66fdc1ab944ceaaed3
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2022-09-26 01:20:06 +0700

    Food Delivery Part 2 = 01:27:00 Example Persistent Bottom Nav Bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:food_delivery_flutter/pages/home/main_food_page.dart';
 import 'package:food_delivery_flutter/utils/colors.dart';
+//import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +15,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _seletectedIndex = 0;
+  //late PersistentTabController _controller;
+
   List pages = [
     MainFoodPage(),
     Container(
@@ -38,10 +42,68 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+
+  //   super.initState();
+  //   _controller = PersistentTabController(initialIndex: 0);
+  // }
+
   @override
   void dispose() {
     super.dispose();
   }
+
+  // List<Widget> _buildScreens() {
+  //   return [
+  //     MainFoodPage(),
+  //     Container(
+  //       child: Center(
+  //         child: Text("Next Page"),
+  //       ),
+  //     ),
+  //     Container(
+  //       child: Center(
+  //         child: Text("Next Next Page"),
+  //       ),
+  //     ),
+  //     Container(
+  //       child: Center(
+  //         child: Text("Next Next Next Page"),
+  //       ),
+  //     ),
+  //   ];
+  // }
+
+  // List<PersistentBottomNavBarItem> _navBarsItems() {
+  //   return [
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.home),
+  //       title: ("Home"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.archivebox_fill),
+  //       title: ("Archive"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.cart_fill),
+  //       title: ("Cart"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.person),
+  //       title: ("Account"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //   ];
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -61,28 +123,65 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(
               Icons.home_outlined,
             ),
-            label: "HOME",
+            label: "Home",
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.archive,
             ),
-            label: "HISTORY",
+            label: "History",
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.shopping_cart,
             ),
-            label: "CART",
+            label: "Cart",
           ),
           BottomNavigationBarItem(
             icon: Icon(
               Icons.person,
             ),
-            label: "ACCOUNT",
+            label: "Account",
           ),
         ],
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return PersistentTabView(
+  //     context,
+  //     controller: _controller,
+  //     screens: _buildScreens(),
+  //     items: _navBarsItems(),
+  //     confineInSafeArea: true,
+  //     backgroundColor: Colors.white, // Default is Colors.white.
+  //     handleAndroidBackButtonPress: true, // Default is true.
+  //     resizeToAvoidBottomInset:
+  //         true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+  //     stateManagement: true, // Default is true.
+  //     hideNavigationBarWhenKeyboardShows:
+  //         true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+  //     decoration: NavBarDecoration(
+  //       borderRadius: BorderRadius.circular(10.0),
+  //       colorBehindNavBar: Colors.white,
+  //     ),
+  //     popAllScreensOnTapOfSelectedTab: true,
+  //     popActionScreens: PopActionScreensType.all,
+  //     itemAnimationProperties: ItemAnimationProperties(
+  //       // Navigation Bar's items animation properties.
+  //       duration: Duration(milliseconds: 200),
+  //       curve: Curves.ease,
+  //     ),
+  //     screenTransitionAnimation: ScreenTransitionAnimation(
+  //       // Screen transition animation on change of selected tab.
+  //       animateTabTransition: true,
+  //       curve: Curves.ease,
+  //       duration: Duration(milliseconds: 200),
+  //     ),
+  //     navBarStyle:
+  //         NavBarStyle.style1, // Choose the nav bar style with this property.
+  //   );
+  // }
 }
